57c1ccc766ef5c6f0eb1a0f010caa7c476760bab
Metro <jessicametro@gmail.com>
2013-04-22 19:01:41 -0500

    split js : model + view + controller

--- www/js/view.js
+++ www/js/view.js
@@ -1,100 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
- 
-
-
-
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-
-
-
-
-// app.initialize();
-   
-   
-   
-   
-$(document).ready(function(){
-	displayRecipeGrid();
-});
-
-
-
-
-function camera(){
-	var settings = {
-		quality: 100,
-		destinationType: Camera.DestinationType.DATA_URL,
-		sourceType: Camera.PictureSourceType.CAMERA,
-		saveToPhotoAlbum: true,
-	};
-	navigator.camera.getPicture(cameraSuccess, cameraFailure, settings);
-}
-
-function cameraSuccess(imageData){
-	console.log("saved photo");
-	document.getElementById('image').src = "data:image/jpeg;base64," + imageData;  //  id might need to be changed to class 'newCoverPhoto'
-}
-
-function cameraFailure(){
-	alert("Error");
-}
-
-
-
-
 function clearRecipeContents() {
 	$('#createRecipe textarea').html('');  // first pass an empty string to clear the html
 }
 
-function createAndSaveRecipe(parentDiv) {
+function createOrEditRecipe(parentDiv) {
 	var recipe = {
 		name: "",
 		yield: "",
@@ -117,14 +25,7 @@ function createAndSaveRecipe(parentDiv) {
 	recipe.tags = parseTags(parentDiv);
 	console.log("recipe saved:");
 	console.log(recipe);
-	window.localStorage.setItem(recipe.name, JSON.stringify(recipe));
-}
-
-function retrieveRecipe(name) {
-	var retrievedRecipe = JSON.parse(window.localStorage.getItem(name));
-	console.log("recipe retrieved:");
-	console.log(retrievedRecipe);
-	return retrievedRecipe;
+	saveRecipe(recipe);
 }
 
 function parseIngredients(parentDiv) {
@@ -190,14 +91,6 @@ function displayEditableRecipe(name) {
 	$('.newInstructions').val(recipe.instructions.join('\n'));
 	$('.newNotes').val(recipe.notes);
 	$('.newTags').val(recipe.tags.join(','));
-}
-
-function retrieveRecipeList() {
-	var recipeArray = [];
-	for (var i = 0; i < window.localStorage.length; i++) {
-		recipeArray.push(window.localStorage.key(i));
-	}
-	return recipeArray;
 }
 
 function displayRecipeGrid() {
